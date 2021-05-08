4106156d0bd065c243fda99033e273a9045a31af
Francisco Groppo <groppofilho@yahoo.com.br>
2021-05-08 17:45:17 -0300

    Inicialização do repositório

---
+++ assembler/tables.py
@@ -0,0 +1,19 @@
+inst_table = {
+    "ADD"   : "0101",
+    "SUB"   : "0110",
+    "LOAD"  : "0100",
+    "STORE" : "0011",
+    "JUMP"   : "1"
+}
+
+space_table = {
+    "code"   : 128,
+    "unused" : 112,
+    "data"   :  16
+}
+
+fields_table = {
+    "opcode"  : 4,
+    "address" : 4,
+    "jump"    : 7
+}
