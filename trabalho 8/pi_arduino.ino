f9d69d769fd5c662f93ed3c56a7e99efcd72e263
Silvana Cardoso <44185240+silvanacardoso@users.noreply.github.com>
2021-06-30 18:49:07 -0300

    Update pi_arduino.ino

--- trabalho 8/pi_arduino.ino
+++ trabalho 8/pi_arduino.ino
@@ -1,22 +1,22 @@
-#define INTERAC 1000000
+#define ITERA 1000000
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
 
 Serial.print("Número de termos da série: ");
-Serial.println(INTERAC);
+Serial.println(ITERA);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  unsigned long interactions = INTERAC, curTime, curTime2;
+  unsigned long iterations = ITERA, curTime, curTime2;
   float x = 1.0;
   float pi = 1.0; //pi inicia o loop igual ao primeiro termo da série de arctg(1)
   
   curTime = millis(); //tempo antes do início do cálculo de pi
   
-  for (unsigned long i = 1; i < interactions; i++){
+  for (unsigned long i = 1; i < iterations; i++){
     
     x*=-1.0; //x = 1.0 se i for par, x = -1.0 se i for ímpar
     pi+= x/(2.0f*(float)i+1.0f); //calcula o i-ésimo termo da série de arctg(1)
